2b6872ce6517fb0c6138ad216c2f9c096775424d
Neville Nazerane <live.neville@live.com>
2024-03-05 23:06:13 -0700

    writes

--- src/WhisperAi.NetConsole/Program.cs
+++ src/WhisperAi.NetConsole/Program.cs
@@ -11,12 +11,12 @@ var fileName = Path.Combine(@"D:\temp\recordings", $"{Guid.NewGuid():N}.wav");
 
 
 
-var modelName = "ggml-SmallEn.bin";
+var modelName = "ggml-MediumEn.bin";
 
 if (!File.Exists(modelName))
 {
     Console.WriteLine("Downloading model...");
-    await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.SmallEn);
+    await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.MediumEn);
     await using var fileWriter = File.OpenWrite(modelName);
     await modelStream.CopyToAsync(fileWriter);
     Console.WriteLine("Done downloading...");
@@ -31,55 +31,25 @@ await using var processor = whisperFactory.CreateBuilder()
                                             .Build();
 
 
+Console.WriteLine("Listening...");
 await RecordingAsync(fileName);
+Console.WriteLine("Done listening");
 var wavFileName = fileName;
 await using var stream = File.OpenRead(wavFileName);
 
 //var stream = new MemoryStream();
 //await KeepRecordingAsync(stream);
 
-Console.WriteLine("Recog started...");
+Console.WriteLine("Processing started...");
 var timeStamp = DateTime.UtcNow;
 await foreach (var result in processor.ProcessAsync(stream))
 {
     Console.WriteLine($"{result.Start}->{result.End}: {result.Text}");
 }
-Console.WriteLine($"Recog done in {(DateTime.UtcNow - timeStamp).TotalMilliseconds}ms");
+Console.WriteLine($"Processing done in {(DateTime.UtcNow - timeStamp).TotalMilliseconds}ms");
 
 
 
-
-async Task KeepRecordingAsync(Stream stream)
-{
-
-    var taskSource = new TaskCompletionSource();
-
-    var waveIn = new WaveInEvent
-    {
-        WaveFormat = new WaveFormat(16000, 16, 1) // 16kHz sample rate, 16 bits per sample, 1 channel (mono)
-    };
-
-    var writer = stream;
-
-    waveIn.DataAvailable += (sender, args) =>
-    {
-        writer.WriteAsync(args.Buffer, 0, args.BytesRecorded);
-        if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 10)
-        {
-            waveIn.StopRecording();
-        }
-    };
-
-    waveIn.RecordingStopped += async (sender, args) =>
-    {
-        waveIn.Dispose();
-        taskSource.TrySetResult();
-    };
-
-    waveIn.StartRecording();
-    await taskSource.Task;
-    Console.WriteLine("Recording started...");
-}
 
 async Task RecordingAsync(string outputFilePath)
 {
