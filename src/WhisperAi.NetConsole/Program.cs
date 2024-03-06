510f2b6be0bb4648da9d92c5e9e0eb1e0e2f1a66
Neville Nazerane <live.neville@live.com>
2024-03-06 00:28:02 -0700

    coded streams

--- src/WhisperAi.NetConsole/Program.cs
+++ src/WhisperAi.NetConsole/Program.cs
@@ -6,22 +6,11 @@ using Whisper.net.Ggml;
 
 
 
-var fileName = Path.Combine(@"D:\temp\recordings", $"{Guid.NewGuid():N}.wav");
-
-
-
 
 var modelName = "ggml-MediumEn.bin";
 
 if (!File.Exists(modelName))
-{
-    Console.WriteLine("Downloading model...");
-    await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.MediumEn);
-    await using var fileWriter = File.OpenWrite(modelName);
-    await modelStream.CopyToAsync(fileWriter);
-    Console.WriteLine("Done downloading...");
-}
-
+    await GetModelAsync(modelName, GgmlType.MediumEn);
 
 using var whisperFactory = WhisperFactory.FromPath(modelName);
 
@@ -32,13 +21,8 @@ await using var processor = whisperFactory.CreateBuilder()
 
 
 Console.WriteLine("Listening...");
-await RecordingAsync(fileName);
+await using var stream = await RecordToFileAsync(10);
 Console.WriteLine("Done listening");
-var wavFileName = fileName;
-await using var stream = File.OpenRead(wavFileName);
-
-//var stream = new MemoryStream();
-//await KeepRecordingAsync(stream);
 
 Console.WriteLine("Processing started...");
 var timeStamp = DateTime.UtcNow;
@@ -49,24 +33,55 @@ await foreach (var result in processor.ProcessAsync(stream))
 Console.WriteLine($"Processing done in {(DateTime.UtcNow - timeStamp).TotalSeconds}s");
 
 
-
-
-async Task RecordingAsync(string outputFilePath)
+async Task<Stream> RecordToStreamAsync(int seconds)
 {
+    var writer = new MemoryStream();
 
-    var taskSource = new TaskCompletionSource();
+    var taskSource = new TaskCompletionSource<Stream>();
 
     var waveIn = new WaveInEvent
     {
         WaveFormat = new WaveFormat(16000, 16, 1) // 16kHz sample rate, 16 bits per sample, 1 channel (mono)
     };
 
-    using var writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+    waveIn.DataAvailable += (sender, args) =>
+    {
+        writer.WriteAsync(args.Buffer, 0, args.BytesRecorded);
+        if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * seconds)
+        {
+            waveIn.StopRecording();
+        }
+    };
+
+    waveIn.RecordingStopped += (sender, args) =>
+    {
+        waveIn.Dispose();
+        taskSource.TrySetResult(writer);
+    };
+
+    waveIn.StartRecording();
+    return await taskSource.Task;
+}
+
+async Task<Stream> RecordToFileAsync(int seconds)
+{
+
+    var fileName = Path.Combine(@"D:\temp\recordings", $"{Guid.NewGuid():N}.wav");
+
+
+    var taskSource = new TaskCompletionSource<Stream>();
+
+    var waveIn = new WaveInEvent
+    {
+        WaveFormat = new WaveFormat(16000, 16, 1) // 16kHz sample rate, 16 bits per sample, 1 channel (mono)
+    };
+
+    using var writer = new WaveFileWriter(fileName, waveIn.WaveFormat);
 
     waveIn.DataAvailable += (sender, args) =>
     {
         writer.WriteAsync(args.Buffer, 0, args.BytesRecorded);
-        if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 10)
+        if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * seconds)
         {
             waveIn.StopRecording();
         }
@@ -76,9 +91,20 @@ async Task RecordingAsync(string outputFilePath)
     {
         await writer.DisposeAsync();
         waveIn.Dispose();
-        taskSource.TrySetResult();
+        var stream = File.OpenRead(fileName);
+
+        taskSource.TrySetResult(stream);
     };
 
     waveIn.StartRecording();
-    await taskSource.Task;
+    return await taskSource.Task;
+}
+
+static async Task GetModelAsync(string modelName, GgmlType ggmlType)
+{
+    Console.WriteLine("Downloading model...");
+    await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
+    await using var fileWriter = File.OpenWrite(modelName);
+    await modelStream.CopyToAsync(fileWriter);
+    Console.WriteLine("Done downloading...");
 }
